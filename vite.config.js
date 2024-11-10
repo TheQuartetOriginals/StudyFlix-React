aa7dd32795404515b9b9b2b66974204e08961434
JaiveerSingh2004 <jaiveer2104.be23@chitkara.edu.in>
2024-11-10 18:50:40 +0530

    Fixed accordian issue for mobile devices

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '192.168.18.30',  // This allows external devices to access the server
+    port: 3000,        // Or whatever port you're using
+  }
 })
